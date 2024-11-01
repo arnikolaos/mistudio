c0f2f2c143962d822ca69483e9facddedeb8a24c
arnikolaos <153008017+arnikolaos@users.noreply.github.com>
2024-11-01 21:26:45 +0200

    contact and index
    fixing contact form

--- contact.js
+++ contact.js
@@ -1,16 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const contactForm = document.getElementById('contact-form');
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
 
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-
-        // Send form data through EmailJS
-        emailjs.sendForm('mistudio', 'mitemplate', this)
-            .then(function() {
-                alert('Message sent successfully!');
-            }, function(error) {
-                alert('Failed to send message. Please try again later.');
-                console.error('Failed...', error);
-            });
-    });
+    emailjs.sendForm('mistudio', 'mitemplate', this)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Message sent successfully!');
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert('Failed to send message. Please try again later.');
+        });
 });
