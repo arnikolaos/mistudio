a5e31418c0fd0089e33349432c747c57ffb6992f
arnikolaos <153008017+arnikolaos@users.noreply.github.com>
2024-12-08 21:11:32 +0200

    d
    d

--- slider.js
+++ slider.js
@@ -1,91 +1,91 @@
-// Project Images
+// List of images for each project
 const projects = [
     {
-        title: 'KL Villa',
+        title: "KL Villa",
         images: [
-            'multimedia/gallery/killini villa 110/villa kl29.jpeg',
-            'multimedia/gallery/killini villa 110/villa kl9.jpeg',
-            'multimedia/gallery/killini villa 110/vill kl23.jpeg'
+            "multimedia/gallery/killini villa 110/villa kl5.jpeg",
+            "multimedia/gallery/killini villa 110/villa kl29.jpeg",
+            "multimedia/gallery/killini villa 110/villa kl30.jpeg"
         ]
     },
     {
-        title: 'Brick House',
+        title: "Brick House",
         images: [
-            'multimedia/gallery/brick house/house out day 2.jpg',
-            'multimedia/gallery/brick house/house1.jpg',
-            'multimedia/gallery/brick house/house2.jpg'
+            "multimedia/gallery/brick house/house out day 2.jpg",
+            "multimedia/gallery/brick house/house1.jpg",
+            "multimedia/gallery/brick house/house2.jpg"
         ]
     },
     {
-        title: 'Kanso Haus',
+        title: "Kanso Haus",
         images: [
-            'multimedia/gallery/kanso haus 簡素/japbath4.jpeg'
+            "multimedia/gallery/kanso haus 簡素/japbath4.jpeg"
         ]
     },
     {
-        title: 'Kalino Residence',
+        title: "Kalino Residence",
         images: [
-            'multimedia/gallery/kalino villa/fireplace1.jpeg'
+            "multimedia/gallery/kalino villa/fireplace1.jpeg"
         ]
     },
     {
-        title: 'Ritso Resort',
+        title: "Ritso Resort",
         images: [
-            'multimedia/gallery/ritso/ RITSO Resort_By MI Architects 17.jpg'
+            "multimedia/gallery/ritso/ RITSO Resort_By MI Architects 17.jpg"
         ]
     },
     {
-        title: 'Domaine des Palmes',
+        title: "Domaine des Palmes",
         images: [
-            'multimedia/gallery/palm villa/villa palms4.jpeg'
+            "multimedia/gallery/palm villa/villa palms4.jpeg"
         ]
     },
     {
-        title: 'Cavaro Estate',
+        title: "Cavaro Estate",
         images: [
-            'multimedia/gallery/kalino villa/kalinovilla0012.jpeg'
+            "multimedia/gallery/kalino villa/kalinovilla0012.jpeg"
         ]
     },
     {
-        title: 'Sculptural Ceiling Building',
+        title: "Sculptural Ceiling Building",
         images: [
-            'multimedia/gallery/sculpture ceiling/Building Sculptural Ceiling_By MI Architects 19.jpg'
+            "multimedia/gallery/sculpture ceiling/Building Sculptural Ceiling_By MI Architects 19.jpg"
         ]
     },
     {
-        title: 'LEB Seaside Villa',
+        title: "LEB Seaside Villa",
         images: [
-            'multimedia/gallery/killini villa big/ilia_villa1.jpg'
+            "multimedia/gallery/killini villa big/ilia_villa1.jpg"
         ]
     },
     {
-        title: 'Beauty Salon',
+        title: "Beauty Salon",
         images: [
-            'multimedia/gallery/co-working moscow/00032.jpeg'
+            "multimedia/gallery/co-working moscow/00032.jpeg"
         ]
     },
     {
-        title: 'Serenity Circle Garden',
+        title: "Serenity Circle Garden",
         images: [
-            'multimedia/gallery/serenity circle garden/tower_meditation-1 2.jpeg'
+            "multimedia/gallery/serenity circle garden/tower_meditation-1 2.jpeg"
         ]
     },
     {
-        title: 'Modern Apartment',
+        title: "Modern Apartment",
         images: [
-            'multimedia/gallery/appartment dinning sofa/dining and sofa3.jpeg'
+            "multimedia/gallery/appartment dinning sofa/dining and sofa3.jpeg"
         ]
     },
     {
-        title: 'Apartment Renovation Voula',
+        title: "Apartment Renovation Voula",
         images: [
-            'multimedia/gallery/renovation voula/apparment renovation voula.jpg'
+            "multimedia/gallery/renovation voula/apparment renovation voula.jpg"
         ]
     },
     {
-        title: 'Infinity Estate',
+        title: "Infinity Estate",
         images: [
-            'multimedia/gallery/infinity villa/infinite_villa.2457.jpeg'
+            "multimedia/gallery/infinity villa/infinite_villa.2457.jpeg"
         ]
     }
 ];
